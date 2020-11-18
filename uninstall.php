c526d8ce3d8367abe30f4501db7965d0bfc02187
Andrea Porotti <53380085+andreaporotti@users.noreply.github.com>
2020-11-18 16:49:19 +0100

    Added plugin settings removal on uninstall.

--- uninstall.php
+++ uninstall.php
@@ -10,3 +10,31 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+// Perform security checks.
+$is_plugin_valid = ( isset( $_REQUEST['plugin'] ) && strpos( sanitize_text_field( wp_unslash( $_REQUEST['plugin'] ) ), 'smart-admin-search' ) !== false ) ? true : false;
+$is_slug_valid   = ( isset( $_REQUEST['slug'] ) && strpos( sanitize_text_field( wp_unslash( $_REQUEST['slug'] ) ), 'smart-admin-search' ) !== false ) ? true : false;
+$is_user_allowed = current_user_can( 'delete_plugins' );
+
+if ( ! $is_plugin_valid || ! $is_slug_valid || ! $is_user_allowed ) {
+	exit;
+}
+
+// Check if plugin settings and data must be removed.
+$option_delete_data_on_uninstall = get_option( 'sas_delete_data_on_uninstall' );
+
+if ( '1' === $option_delete_data_on_uninstall ) {
+
+	// Delete options.
+	$options = array(
+		'sas_search_keys_shortcut',
+		'sas_disabled_search_functions',
+		'sas_delete_data_on_uninstall',
+	);
+
+	foreach ( $options as $option ) {
+		if ( get_option( $option ) ) {
+			delete_option( $option );
+		}
+	}
+}
